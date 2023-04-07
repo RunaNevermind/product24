7c74edb6571961c9c2b1297d1e0b305589592cde
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2023-04-07 18:10:16 +0500

    Done

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+"use strict"
+
+$(document).ready(function(){
+    $(".slider").bxSlider();
+});
+
+window.onload = function(){
+    const paralax = document.querySelector('.container__main')
+    if(paralax){
+        const container = document.querySelector('.paralax__container')
+        const ballsLeft = document.querySelector('.balls__left');
+        const ballsRight = document.querySelector('.balls__right');
+
+        const forBalls=30
+
+        const speed = 0.08;
+
+        let positionX= 0;
+        let positionY = 0;
+
+        let coordXpercent=0;
+        let coordYpercent=0;
+
+        function setMouseParalaxStyle(){
+            const distX= coordXpercent - positionX;
+            const distY= coordYpercent - positionY;
+
+            positionX = positionX + (distX * speed);
+            positionY = positionY + (distY * speed);
+
+            ballsLeft.style.cssText = `transform: translate(${positionX / forBalls}%,${positionY / forBalls}%);`;
+            ballsRight.style.cssText = `transform: translate(${positionX / forBalls}%,${positionY / forBalls}%);`;
+
+            requestAnimationFrame(setMouseParalaxStyle);
+        }
+        setMouseParalaxStyle()
+
+        paralax.addEventListener('mousemove', function (e){
+            const paralaxWidth = paralax.offsetWidth;
+            const paralaxHeight = paralax.offsetHeight;
+
+            const coordX = e.pageX - paralaxWidth/2;
+            const coordY = e.pageY - paralaxHeight/2;
+
+            coordXpercent= coordX / paralaxWidth * 100;
+            coordYpercent= coordY / paralaxHeight * 100;
+            console.log(coordXpercent , coordYpercent)
+        })
+    }
+
+
+
+}
